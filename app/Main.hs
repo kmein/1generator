e97232e8a61aa3601235d255a751612af7066ffb
kmein <kieran.meinhardt@gmail.com>
2017-05-02 22:47:17 +0200

    Parse replacements from csv

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,19 @@
 module Main where
 
-import Vong
-import qualified Data.Text.IO as T
+import Data.Csv (decode, HasHeader(NoHeader))
+import Data.Foldable (toList)
+import Vong (Replacement, translate)
+import qualified Data.ByteString.Lazy as B (ByteString, readFile)
+import qualified Data.Text.IO as T (getContents, putStrLn)
+
+parseReplacements :: B.ByteString -> [Replacement]
+parseReplacements csvData =
+    case decode NoHeader csvData of
+        Right xs -> toList xs
+        Left _ -> []
 
 main :: IO ()
-main = T.getContents >>= translate >>= T.putStrLn
+main = do
+    repls <- parseReplacements <$> B.readFile "vong.csv"
+    T.getContents >>= translate repls >>= T.putStrLn
+
--- src/Vong.hs
+++ src/Vong.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DeriveGeneric #-}
 module Vong where
 
 import Control.Monad (foldM)
+import Data.Csv (FromRecord)
+import GHC.Generics (Generic)
 import System.Random (randomRIO)
-import qualified Data.Text as T
+import qualified Data.Text as T (Text, replace)
 
 type Probability = Float
 
@@ -12,7 +14,9 @@ data Replacement = Replacement
     { search :: T.Text
     , replace :: T.Text
     , probability :: Probability
-    }
+    } deriving (Generic)
+
+instance FromRecord Replacement
 
 conditioned :: Probability -> IO Bool
 conditioned p = (1 - p <=) <$> randomRIO (0, 1)
@@ -25,65 +29,5 @@ replaceConditioned text Replacement{..} = do
             then T.replace search replace text
             else text
 
-translate :: T.Text -> IO T.Text
-translate text = foldM replaceConditioned text replacements
-  where
-    replacements =
-        [ Replacement " eine " " 1 " 1
-        , Replacement " ein " " 1 " 1
-        , Replacement " zwei " "2 " 1
-        , Replacement " drei " "3 " 1
-        , Replacement " vier " "4 " 1
-        , Replacement " fünf " "5 " 1
-        , Replacement " sechs " "6 " 1
-        , Replacement " sieben " "6 " 1
-        , Replacement " acht " "6 " 1
-        , Replacement " neun " "6 " 1
-        , Replacement " zehn " "6 " 1
-        , Replacement "Eine " " 1 " 1
-        , Replacement "Ein " " 1 " 1
-        , Replacement "Zwei " "2 " 1
-        , Replacement "Drei " "3 " 1
-        , Replacement "Vier " "4 " 1
-        , Replacement "Fünf " "5 " 1
-        , Replacement "Sechs " "6 " 1
-        , Replacement "Sieben " "6 " 1
-        , Replacement "Acht " "6 " 1
-        , Replacement "Neun " "6 " 1
-        , Replacement "Zehn " "6 " 1
-        , Replacement "ä" "e" 0.5
-        , Replacement "ö" "öh" 0.5
-        , Replacement "ü" "üh" 0.5
-        , Replacement "ins" "in" 0.5
-        , Replacement "sisch" "schis" 0.5
-        , Replacement "stisch" "schtis" 0.5
-        , Replacement "ar" "ahr" 0.5
-        , Replacement "ck" "k" 0.5
-        , Replacement "en" "n" 0.2
-        , Replacement "ft" "f" 0.5
-        , Replacement "hn" "n" 0.5
-        , Replacement "m" "n" 0.8 -- TODO Nur innerhalb eines Wortes erlauben
-        , Replacement "n" "m" 0.8 -- TODO Nur innerhalb eines Wortes erlauben
-        , Replacement "ntsch" "nsch" 0.5
-        , Replacement "ph" "f" 0.5
-        , Replacement "sch" "shc" 0.5
-        , Replacement "sh" "sch" 0.5
-        , Replacement "st" "s" 0.5
-        , Replacement "ß" "s" 0.5
-        , Replacement "tt" "td" 0.5
-        , Replacement "th" "tt" 0.5
-        , Replacement "tzt" "ts" 0.5
-        , Replacement "tz" "ts" 0.5
-        , Replacement "ur" "uhr" 0.5
-        , Replacement "v" "f" 0.5
-        , Replacement "v" "w" 0.5
-        , Replacement "," "" 1
-        , Replacement "seid " "seit " 0.5
-        , Replacement "al " "ahl " 0.5
-        , Replacement "aber" "aba" 0.5
-        , Replacement " an " " in " 0.5
-        , Replacement "auen " "aun " 0.5
-        , Replacement "on " "ong " 0.8
-        , Replacement "y " "i " 0.5
-        , Replacement "chnet " "chnen " 0.3
-        , Replacement "chnen " "chnet " 0.3]
+translate :: [Replacement] -> T.Text -> IO T.Text
+translate = flip (foldM replaceConditioned)
